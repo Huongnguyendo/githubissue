8cc12f3108934ef944fc3f592f4e74c1c569eaa0
linhle <lklinh@outlook.com>
2020-09-25 18:11:26 +0700

    addModal

--- src/issueList/index.js
+++ src/issueList/index.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ListItem from "./listItem/index";
 
 function IssueList({ data }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   if (!data) {
     return <></>;
   }
   return (
     <div>
+      <ListItem show={show} onHide={handleClose} handleClose={handleClose} />
       <ol>
         {data.map((item) => (
-          <li>{item.title}</li>
+          <li onClick={handleShow}>{item.title}</li>
         ))}
       </ol>
     </div>
